e3959e75edd5cffbec73221fafe5f65138252ff2
Yan Smaliak <ysmaliak@gmail.com>
2024-12-15 16:15:51 +0100

    Initial Commit

---
+++ ComposableArchitectureSharingTest/ComposableArchitectureSharingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureSharingTestApp.swift
+//  ComposableArchitectureSharingTest
+//
+//  Created by Yan Smaliak on 15/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureSharingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposableArchitectureSharingTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ComposableArchitectureSharingTest
+//
+//  Created by Yan Smaliak on 15/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
